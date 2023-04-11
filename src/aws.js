21fe89b651af865dea1bf8dbd6c727cc171d677a
Kacper Lodzinski <40675937+kacperlsky@users.noreply.github.com>
2023-04-11 12:06:31 +0100

    Update aws.js

--- src/aws.js
+++ src/aws.js
@@ -9,6 +9,7 @@ function buildUserDataScript(githubRegistrationToken, label) {
     // to be pre-installed in the AMI, so we simply cd into that directory and then start the runner
     return [
       '#!/bin/bash',
+      'sudo iptables -F',
       `cd "${config.input.runnerHomeDir}"`,
       'export RUNNER_ALLOW_RUNASROOT=1',
       'sudo yum install libicu -y',
@@ -18,6 +19,7 @@ function buildUserDataScript(githubRegistrationToken, label) {
   } else {
     return [
       '#!/bin/bash',
+      'sudo iptables -F',
       'mkdir actions-runner && cd actions-runner',
       'case $(uname -m) in aarch64) ARCH="arm64" ;; amd64|x86_64) ARCH="x64" ;; esac && export RUNNER_ARCH=${ARCH}',
       'curl -O -L https://github.com/actions/runner/releases/download/v2.299.1/actions-runner-linux-${RUNNER_ARCH}-2.299.1.tar.gz',
